3438bb2847ce7742d7f1dd45d60c4be19b153eac
Huaying Tsai <royal3501@gmail.com>
2017-09-24 17:13:19 -0700

    make argument

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 from inscrawler import InsCrawler
+import sys
 import argparse
 
 # ins_crawler = InsCrawler()
@@ -22,7 +23,7 @@ def usage():
 
 def get_posts_by_user(username, number=None):
     ins_crawler = InsCrawler()
-    return ins_crawler.get_user_post(username, number)
+    return ins_crawler.get_user_posts(username, number)
 
 
 def get_profile(username):
@@ -35,6 +36,13 @@ def get_posts_by_hashtag(tag, number):
     return ins_crawler.get_latest_posts_by_tag(tag, number)
 
 
+def arg_required(args, fields=[]):
+    for field in fields:
+        if not getattr(args, field):
+            parser.print_help()
+            sys.exit()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Instagram Crawler',
                                      usage=usage())
@@ -45,15 +53,20 @@ if __name__ == '__main__':
                         help='number of returned posts')
     parser.add_argument('-u', '--username',
                         help='instagram\'s username')
+    parser.add_argument('-t', '--tag',
+                        help='instagram\'s tag name')
     parser.add_argument('-o', '--output', help='output file name(json format)')
     args = parser.parse_args()
 
     if args.mode == 'posts':
-        get_posts_by_user()
+        arg_required('username')
+        print(get_posts_by_user(args.username, args.number))
     elif args.mode == 'profile':
-        get_profile()
+        arg_required('username')
+        print(get_profile(args.username))
     elif args.mode == 'hashtag':
-        get_posts_by_hashtag()
+        arg_required('tag')
+        print(get_posts_by_hashtag(args.tag, args.number))
     else:
         usage()
 
