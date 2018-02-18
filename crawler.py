4297dad4c545166fd6b8a904ffe82dfbbd2d1fad
huaying <royal3501@gmail.com>
2018-02-17 20:07:44 -0800

    update usage

--- crawler.py
+++ crawler.py
@@ -9,7 +9,9 @@ def usage():
     return '''
         python crawler.py posts -u cal_foodie -n 100 -o ./output
         python crawler.py profile -u cal_foodie -o ./output
-        python crawler.py hashtag taiwan -o ./output
+        python crawler.py hashtag -t taiwan -o ./output
+
+        The default number for fetching posts via hashtag is 100.
     '''
 
 
