aef2e55fe63b34587b9ddb024d98556f9322cb85
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2019-09-30 18:07:33 +0300

    ...

--- update.py
+++ update.py
@@ -5,24 +5,33 @@ from inscrawler import InsCrawler
 DOMAIN = 'http://miratcan.pythonanywhere.com'
 
 def get_pending_usernames():
-    import ipdb; ipdb.set_trace()
     for record in requests.get(urljoin(DOMAIN,
         '/api/account_status/')).json()['accounts']:
         yield record['username'], record['last_post_url']
 
-crawler = InsCrawler()
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    t = range(0, len(l), n)
+    for i in t:
+        yield i, len(t), l[i:i + n]
+
+crawler = InsCrawler()
 for username, stop_post in get_pending_usernames():
-    if username != 'rterdogan':
-        continue
     print(f'Collecting missing posts for {username}')
-    # import json
-    # posts = json.load(open('/tmp/posts.json', 'r'))
-    posts = crawler.get_user_posts(username, stop_post)
+    if not stop_post:
+        continue
+    posts = crawler.get_user_posts(username)
+
     for post in posts:
         post['post_url'] = post.pop('key')
         post['owner_id'] = username
-    import ipdb; ipdb.set_trace()
-    response = requests.post(urljoin(DOMAIN, '/api/update_posts/'),
-            json={'posts': posts})
-    print(response.content)
+        post['thmb_srcset'] = ''
+ 
+    for counter, total, chunk in chunks(posts, 10):
+        print(f'Updating {counter + 1}/{total}')
+        response = requests.post(urljoin(DOMAIN, '/api/update_posts/'),
+                                 json={'posts': chunk}, timeout=60000)
+        print(response.content)
+        import time
+        time.sleep(1)
